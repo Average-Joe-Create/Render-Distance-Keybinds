7c69ac3611903bc4a0c412d0918929c12d7b764b
Average-Joe-Create <ne1butjoe@gmail.com>
2026-06-21 15:59:21 -0600

    Add files via upload

---
+++ src/client/java/render_distance_keybinds/client/ModKeybinds.java
@@ -0,0 +1,51 @@
+package render_distance_keybinds.client;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.network.chat.Component;
+import org.lwjgl.glfw.GLFW;
+
+public class ModKeybinds {
+
+    private static KeyMapping increaseRenderDistance;
+    private static KeyMapping decreaseRenderDistance;
+
+    public static void register() {
+        increaseRenderDistance = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+            "key.render-distance.increase",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_BRACKET,
+            "key.category.render-distance.keycategory"
+        ));
+
+        decreaseRenderDistance = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+            "key.render-distance.decrease",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_LEFT_BRACKET,
+            "key.category.render-distance.keycategory"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (increaseRenderDistance.consumeClick()) {
+                int current = client.options.renderDistance().get();
+                if (current < 32) {
+                    client.options.renderDistance().set(current + 1);
+                    client.gui.setOverlayMessage(Component.translatable("msg.render-distance.increased", (current + 1)), false);
+                } else {
+                    client.gui.setOverlayMessage(Component.translatable("msg.render-distance.max"), false);
+                }
+            }
+            while (decreaseRenderDistance.consumeClick()) {
+                int current = client.options.renderDistance().get();
+                if (current > 2) {
+                    client.options.renderDistance().set(current - 1);
+                    client.gui.setOverlayMessage(Component.translatable("msg.render-distance.decreased", (current - 1)), false);
+                } else {
+                    client.gui.setOverlayMessage(Component.translatable("msg.render-distance.min"), false);
+                }
+            }
+        });
+    }
+}
